fae94e8ff62531ecb24c146cee197e4f63c14c5c
hongminpark <>
2023-07-20 19:23:00 +0900

    Fix first duplicated loading, add infinite loading

---
+++ src/components/ImageFeed.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect, useRef, useState } from "react";
+import InfiniteScroll from "react-infinite-scroll-component";
+import { fetchStickers } from "../supabase";
+
+interface Sticker {
+  id: number;
+  image_url: string;
+}
+
+const ImageFeed: React.FC = () => {
+  const [stickers, setStickers] = useState<Sticker[]>([]);
+  const [hasMore, setHasMore] = useState(true);
+  const initialFetchDone = useRef(false);
+
+  const fetchMoreData = async () => {
+    if (initialFetchDone.current) {
+      const newStickers = await fetchStickers(stickers.length, 64);
+      setStickers((prevStickers) => [...prevStickers, ...newStickers]);
+      if (newStickers.length === 0) {
+        setHasMore(false);
+      }
+    } else {
+      initialFetchDone.current = true;
+    }
+  };
+
+  useEffect(() => {
+    fetchMoreData();
+  }, []);
+
+  return (
+    <InfiniteScroll
+      dataLength={stickers.length}
+      next={fetchMoreData}
+      hasMore={hasMore}
+      loader={<h4>Loading...</h4>}
+      endMessage={
+        <p style={{ textAlign: "center" }}>
+          <b>Yay! You have seen it all</b>
+        </p>
+      }
+    >
+      <div className="grid grid-cols-2 gap-px bg-black sm:grid-cols-4 md:grid-cols-6 lg:grid-cols-8">
+        {stickers.map((sticker) => (
+          <div
+            className="relative border-b border-r border-white last:border-b-0 last:border-r-0"
+            style={{ paddingBottom: "120%" }}
+          >
+            <div className="absolute left-1/4 top-1/4 flex h-1/2 w-1/2 items-center justify-center">
+              <img
+                src={sticker.image_url}
+                alt=""
+                className="max-h-full max-w-full cursor-pointer object-contain transition-transform duration-300 ease-in-out hover:scale-110"
+              />
+            </div>
+          </div>
+        ))}
+      </div>
+    </InfiniteScroll>
+  );
+};
+export default ImageFeed;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -3,7 +3,14 @@ import { type Config } from "tailwindcss";
 export default {
   content: ["./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['"Helvetica Neue"', "Arial", "sans-serif"],
+      },
+      maxHeight: {
+        "1/2": "50%",
+      },
+    },
   },
   plugins: [],
 } satisfies Config;
